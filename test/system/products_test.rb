460fe678f16b8e7c86accaed191994ad4eff564b
pustoj0 <pustoj0@gmail.com>
2020-08-21 20:55:23 +0300

    Dynamical Form

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,8 +1,16 @@
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
   setup do
-    @product = products(:one)
+    @product = products(:ruby)
   end
 
   test "visiting the index" do
@@ -20,7 +28,7 @@ class ProductsTest < ApplicationSystemTestCase
     fill_in "Title", with: @product.title
     click_on "Create Product"
 
-    assert_text "Product was successfully created"
+    assert_text "Title has already been taken"
     click_on "Back"
   end
 
@@ -31,7 +39,7 @@ class ProductsTest < ApplicationSystemTestCase
     fill_in "Description", with: @product.description
     fill_in "Image url", with: @product.image_url
     fill_in "Price", with: @product.price
-    fill_in "Title", with: @product.title
+    fill_in "Title", with: "Karel The Robot in a Nutshell"
     click_on "Update Product"
 
     assert_text "Product was successfully updated"
